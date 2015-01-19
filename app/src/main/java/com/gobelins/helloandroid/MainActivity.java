f3fd43a69253980c7999ed45cbf1197a7cbe11d8
NoemieD <noemiediaz4@gmail.com>
2015-01-19 10:28:00 +0100

    Ajouter un btn Ajouter un txt

--- app/src/main/java/com/gobelins/helloandroid/MainActivity.java
+++ app/src/main/java/com/gobelins/helloandroid/MainActivity.java
@@ -2,8 +2,10 @@ package com.gobelins.helloandroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Log.d("DEBUG_UI", view.toString());
     }
 }
